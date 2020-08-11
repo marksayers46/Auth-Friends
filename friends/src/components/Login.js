13ec7cc2449f61b413dc75db9718e2163fcfe763
Mark Sayers <marksayers46@icloud.com>
2020-08-11 15:10:05 -0500

    setup Login.js

---
+++ friends/src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { axiosWithAuth } from "../axiosWithAuth";
+
+const Login = () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    return(
+        <>
+        
+        </>
+    );
+};
+
+
+
